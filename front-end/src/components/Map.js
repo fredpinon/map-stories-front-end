eb83957f8e261c5c93ce8fe64a63b13b1880cf79
KarinaDavtyan <k.davtyan27@gmail.com>
2017-10-10 21:28:57 +0200

    feat: map addLayer

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -4,22 +4,97 @@ import MapboxGeocoder from 'mapbox-gl-geocoder';
 
 class Map extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      coordinates: {}
+    };
+
+    this.geoJson = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+  }
+
+  // {
+  //   "type": "Feature",
+  //   "properties": {},
+  //   "geometry": {
+  //     "type": "Point",
+  //     "coordinates": [
+  //       e.lngLat.lng,
+  //       e.lngLat.lat,
+  //     ]
+  //   }
+  // }
+
   componentDidMount () {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYWNvbGxpbnM4NSIsImEiOiJjajhnMGZwYzMwOHBxMnhxajd0aWppbWE5In0.i6PUo_ai7q6NeIWBFPtGKA';
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/outdoors-v10',
       center: [2.15, 41.36],
-      zoom: 7
+      zoom: 9
     });
-
     this.map.doubleClickZoom.disable();
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken
     });
     this.map.addControl(geocoder, 'top-right');
+    this.map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+          enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }));
 
-    }
+    this.map.on('load', (e) => {
+      this.map.addSource('markers', {
+        type: 'geojson',
+        data: this.geoJson
+      });
+      this.map.addLayer({
+        id: 'carina',
+        type: 'circle',
+        paint: {
+          'circle-radius': 8,
+          'circle-color': 'purple'
+        },
+        source: 'markers',
+      });
+    });
+
+    this.map.on('click', (e) => {
+      // this.setState({
+      //   coordinates: {
+      //     lng: e.lngLat.lng,
+      //     lat: e.lngLat.lat
+      //   }
+      // });
+
+      if (this.geoJson.features.length === 0) {
+        this.geoJson.features.push({
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "Point",
+            coordinates: [
+              e.lngLat.lng,
+              e.lngLat.lat,
+            ]
+          }
+        });
+      } else {
+        this.geoJson.features[0].geometry.coordinates = [
+          e.lngLat.lng,
+          e.lngLat.lat,
+        ]
+      }
+      console.log(this.geoJson.features);
+    })
+  }
+
+
 
   render () {
     return (
