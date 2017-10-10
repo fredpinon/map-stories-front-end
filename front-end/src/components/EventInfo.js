d1399fc81b36195cc817e60f6a885d121299d7d0
Anna Collins <annacollins85@gmail.com>
2017-10-10 17:37:37 +0200

    feat: input fields and buttons added for saving event and story

--- front-end/src/components/EventInfo.js
+++ front-end/src/components/EventInfo.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import '../css/EventInfoContainer.css';
+import '../css/EditorPage.css';
+
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
 
 class EventInfo extends Component {
 
@@ -10,11 +12,18 @@ class EventInfo extends Component {
       width: 550,
       margin: 40,
       textAlign: 'center',
-      display: 'inline-block',
     };
     return (
       <div className="EventInfoContainer">
-        <Paper style={style} zDepth={3} />
+        <Paper style={style} zDepth={3}>
+          <TextField hintText="MM:SS" floatingLabelText="Time for event to start"/><br />
+          <TextField hintText="Title" floatingLabelText="Title"/><br />
+          <TextField hintText="Location (optional)" floatingLabelText="Location"/><br />
+          <TextField hintText="Description" floatingLabelText="Description" multiline={true} rows={3} rowsMax={4} wrap={true}/><br />
+          <TextField hintText="Tweet (optional)" floatingLabelText="Tweet"/><br />
+          <TextField hintText="Image URL (optional)" floatingLabelText="Image URL"/><br />
+          <TextField hintText="YouTube URL (optional)" floatingLabelText="YouTubeURL"/><br />
+        </Paper>
       </div>
     );
   }
--- front-end/src/containers/EditorPage.js
+++ front-end/src/containers/EditorPage.js
@@ -3,13 +3,22 @@ import '../css/EditorPage.css';
 
 import Map from '../components/Map';
 import EventInfo from '../components/EventInfo';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class EditorPage extends Component {
 
   render () {
+    const styleButton = {
+      marginRight: 30,
+      marginBottom: 30,
+    }
     return (
       <div className="EditorPage">
-        <Map/>
+        <EventInfo/>
+        <div className="buttons">
+          <RaisedButton label="Save Event" primary={true} style={styleButton}/>
+          <RaisedButton label="Save and Publish Story" primary={true} style={styleButton}/>
+        </div>
       </div>
     )
   }
