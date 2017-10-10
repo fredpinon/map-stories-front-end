d76a8d0edac9abc1633be5dbbf89e966584585fe
Anna Collins <annacollins85@gmail.com>
2017-10-10 22:08:20 +0200

    feat: attachment input field rendering when attachment type selected

--- front-end/src/components/EventInfo.js
+++ front-end/src/components/EventInfo.js
@@ -3,26 +3,53 @@ import '../css/EditorPage.css';
 
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+
 
 class EventInfo extends Component {
 
+  state = {
+    value: null,
+  };
+
+  handleChange = (event, index, value) => {
+    this.setState({value});
+  }
+
+  renderInputField = () => {
+    if (!this.state.value) return null;
+    return <TextField hintText="Add..." floatingLabelText="Add..." fullWidth={true}/>
+  }
+
   render() {
-    const style = {
-      height: 950,
-      width: 550,
-      margin: 40,
-      textAlign: 'center',
-    };
     return (
       <div className="EventInfoContainer">
-        <Paper style={style} zDepth={3}>
-          <TextField hintText="MM:SS" floatingLabelText="Time for event to start"/><br />
-          <TextField hintText="Title" floatingLabelText="Title"/><br />
-          <TextField hintText="Location (optional)" floatingLabelText="Location"/><br />
-          <TextField hintText="Description" floatingLabelText="Description" multiline={true} rows={3} rowsMax={4} wrap={true}/><br />
-          <TextField hintText="Tweet (optional)" floatingLabelText="Tweet"/><br />
-          <TextField hintText="Image URL (optional)" floatingLabelText="Image URL"/><br />
-          <TextField hintText="YouTube URL (optional)" floatingLabelText="YouTubeURL"/><br />
+        <Paper className="InputInfo" zDepth={3}>
+          <TextField hintText="MM:SS" floatingLabelText="Time for event to start" fullWidth={true}/><br />
+          <TextField hintText="Event Title" floatingLabelText="Event Title" style={{ fontSize: '24px' }}  fullWidth={true}/><br />
+          <TextField hintText="Date & Time (optional)" floatingLabelText="Date & Time" fullWidth={true}/><br />
+          <TextField hintText="Map Location" floatingLabelText="Map Location" fullWidth={true}/><br />
+          <Divider style={{
+            width: '112%',
+            marginLeft: -30,
+            marginTop: 60,
+          }} />
+          <SelectField
+          floatingLabelText="Attachment"
+          value={this.state.value}
+          onChange={this.handleChange}
+          fullWidth={true}>
+          <MenuItem value={1} primaryText="Text" />
+          <MenuItem value={2} primaryText="Image URL" />
+          <MenuItem value={3} primaryText="Video URL" />
+          <MenuItem value={4} primaryText="Audio URL" />
+        </SelectField>
+        <br />
+        {this.renderInputField()}
+        <FlatButton className="AddAttachment" label="+ Add Another Attachment" primary={true} />
         </Paper>
       </div>
     );
