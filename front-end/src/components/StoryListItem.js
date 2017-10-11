f2e21d21c3b80e7ae08786f11effac9071681ffe
KarinaDavtyan <k.davtyan27@gmail.com>
2017-10-11 21:58:06 +0200

    refactor: added dialog to the buttons

--- front-end/src/components/StoryListItem.js
+++ front-end/src/components/StoryListItem.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class StoryListItem extends Component {
+
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
 
   renderStoryAssets = () => {
     const { title, tagLine, editor } = this.props.story;
@@ -16,29 +31,77 @@ class StoryListItem extends Component {
     )
   }
 
+  // onClickedButton (e) {
+  //   this.handleOpen;
+  //   e.preventDefault();
+  // }
+
   render() {
+    const actionsDelete = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Delete"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+    ];
+    const actionsPublish = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Publish"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+    ];
+
     const style = {
       height: 80,
       width: '100%'
     };
+
     return (
       <div className="StoryListItem">
         <Paper className="Paper" style={style} zDepth={1} children={this.renderStoryAssets()}/>
         <div className='Buttons'>
           <RaisedButton
-            target='_blank'
             label='DELETE'
             onClick={(e) => {
               e.preventDefault();
+              this.setState({open: true});;
             }}
-          />
+          >
+            <Dialog
+           title="Are you sure you want to delete?"
+           actions={actionsDelete}
+           modal={true}
+           open={this.state.open}
+            >
+            Story cannot be restored later
+            </Dialog>
+          </RaisedButton>
           <RaisedButton
-            target='_blank'
             label='PUBLISH'
             onClick={(e) => {
               e.preventDefault();
+              this.setState({open: true});;
             }}
-          />
+          >
+            <Dialog
+           title="Are you sure you want to publish?"
+           actions={actionsPublish}
+           modal={true}
+           open={this.state.open}
+            >
+            </Dialog>
+          </RaisedButton>
         </div>
       </div>
     );
