c1b580eb7c58d19a009917ae0d7e2a0e9cd7e7c8
Anna Collins <annacollins85@gmail.com>
2017-10-12 17:37:02 +0200

    chore: conflicts resolved

--- front-end/src/containers/EditorPage.js
+++ front-end/src/containers/EditorPage.js
@@ -1,23 +1,53 @@
 import React, {Component} from 'react';
 import '../css/EditorPage.css';
+import { editStory } from '../actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import Map from '../components/Map';
-import EventInfo from '../components/EventInfo';
+import EventInfo from './EventInfo';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class EditorPage extends Component {
+  state = {
+    currentEvent: {}
+  }
+
+  constructor (props) {
+    super(props);
+    console.log('props in editor page', props);
+    if(props.story.events.length > 0) {
+      this.state.currentEvent = props.story.events[props.story.events.length-1]
+    }
+  }
+
+  onEventDelete = (event) => {
+
+  }
+
+  onEventSave = (event) => {
+    this.props.addEvent(event);
+  }
 
   render () {
-    const styleButton = {
-      marginRight: 30,
-      marginBottom: 30,
-    }
     return (
       <div className="EditorPage">
-        <EventInfo/>
+        <EventInfo
+          event={this.state.currentEvent}
+          onEventSave={this.onEventSave}
+          onEventDelete={this.onEventDelete}
+        />
       </div>
     )
   }
 }
 
-export default EditorPage;
+const mapStateToProps = (state, ownProps) => ({
+  story: state.entities.stories[ownProps.computedMatch.params.storyId],
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addEvent: (data) => dispatch(editStory(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditorPage);
